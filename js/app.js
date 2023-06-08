bf3256a608f5e12109dc39f6f0cd3703900eb7ea
Dmitry Bobryshev <dmitry.bobryshev@brackets.sk>
2023-06-08 14:06:36 +0200

    feat(index): created prompt slider

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
-const swiper = new Swiper('.cards-slider', {
+const aiCardsSlider = new Swiper('.cards-slider', {
     slidesPerView: 5,
     spaceBetween: 10,
     loop: true,
     speed: 1000,
     autoplay: {
         delay: 3000,
+    },
+    allowTouchMove: false,
+})
+
+const aiPromptSlider = new Swiper('.prompt-text-slider', {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
     },
     allowTouchMove: false,
 })
