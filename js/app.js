a36794506c0be6b922cadade505742cd914bfc15
Dmitry Bobryshev <dmitry.bobryshev@brackets.sk>
2023-06-09 17:03:05 +0200

    feat(index): implement main slider

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 const aiCardsSlider = new Swiper('.cards-slider', {
     slidesPerView: 5,
-    spaceBetween: 10,
+    spaceBetween: 30,
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    allowTouchMove: false,
+})
+
+const aiMainSwiperSlider = new Swiper('.main-swiper-slider', {
     loop: true,
     speed: 1000,
     autoplay: {
@@ -20,4 +29,7 @@ const aiPromptSlider = new Swiper('.prompt-text-slider', {
         crossFade: true
     },
     allowTouchMove: false,
+    thumbs: {
+        swiper: aiMainSwiperSlider,
+    },
 })
